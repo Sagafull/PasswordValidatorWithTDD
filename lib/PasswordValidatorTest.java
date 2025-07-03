6813722ffcb30bc7db5b21d9b206eba7f5801879
Sagafull <Manasawin.l@ku.th>
2025-07-03 19:19:20 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -13,7 +13,70 @@ public class PasswordValidatorTest{
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
         }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+        PasswordStrength result2 = PasswordValidator.validate("12345678");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: 8 Digits or more ");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected WEAK but got " + result2);
+        }
+
+        PasswordStrength result3 = PasswordValidator.validate("12345678A");
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: Contain 8 Digits or more with Uppercase");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected MEDIUM but got " + result3);
+        }
+
+        PasswordStrength result4 = PasswordValidator.validate("12345678a");
+        if (result4 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 4 Passed: Contain 8 Digits or more with Lowercase");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected MEDIUM but got " + result4);
+        }
+
+        PasswordStrength result5 = PasswordValidator.validate("12345678Aa");
+        if (result5 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 5 Passed: Contain 8 Digits or more with Uppercase and Lowercase");
+        } else {
+            System.out.println("Test Case 5 FAILED: Expected STRONG but got " + result5);
+        }
+
+        PasswordStrength result6 = PasswordValidator.validate("12345678Aa@");
+        if (result6 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 6 Passed: Contain 8 Digits or more with Uppercase and Lowercase plus Special Character");
+        } else {
+            System.out.println("Test Case 6 FAILED: Expected STRONG but got " + result6);
+        }
+
+        PasswordStrength result7 = PasswordValidator.validate("AAAAAAAAAA");
+        if (result7 == PasswordStrength.INVALID) {
+            System.out.println("Test Case 7 Passed: INVALID must contain 8 digits ");
+        } else {
+            System.out.println("Test Case 7 FAILED: Expected INVALID but got " + result7);
+        }
+
+        PasswordStrength result8 = PasswordValidator.validate("          ");
+        if (result8 == PasswordStrength.INVALID) {
+            System.out.println("Test Case 8 Passed: INVALID of blank");
+        } else {
+            System.out.println("Test Case 8 FAILED: Expected INVALID but got " + result8);
+        }
+
+        PasswordStrength result9 = PasswordValidator.validate("          12345678");
+        if (result9 == PasswordStrength.INVALID) {
+            System.out.println("Test Case 9 Passed: INVALID must not have white space");
+        } else {
+            System.out.println("Test Case 9 FAILED: Expected INVALID but got " + result9);
+        }
+
+        PasswordStrength result10 = PasswordValidator.validate("12345678   ");
+        if (result10 == PasswordStrength.INVALID) {
+            System.out.println("Test Case 10 Passed: INVALID must not have white space");
+        } else {
+            System.out.println("Test Case 10 FAILED: Expected INVALID but got " + result10);
+        }
+
+
 
         System.out.println("--------------------------------");
     }
